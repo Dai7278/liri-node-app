eb15e74ead419367477c64b17767fd28f1de82b8
Dereck Jones <dereck.it83@gmail.com>
2017-04-12 15:07:09 -0400

    first

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'uM3sa57r4dT69dPFpqek9RJD4',
+  consumer_secret: '5xruMh7mxEfkJEzypDANIilbf3MoomF9R63O8VAyHLGyzEPAFF',
+  access_token_key: '	852221137241202688-aw6F9rMXDRkDXCBQ7Ck4iKwiAoEyv8u',
+  acces_token_secret: 'UFe51jYbYcsijZlo7kyGtCrLRSaWySURUhlqwtxqg323X',
+}
---
+++ liri.js
@@ -0,0 +1,3 @@
+// code you need to grab the data from keys.js
+var keys = require('./keys');
+var twitter = require('twitter');
